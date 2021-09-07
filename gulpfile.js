b2545e76360cbd9139846e4c24004663f451f8dd
karanvp <65488349+karanvp@users.noreply.github.com>
2021-09-07 16:35:05 +0530

    Adding Gulp Usage

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,47 @@
 var gulp = require('gulp');
 
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
 
-//  const sass = require('gulp-sass')(require('sass'));;
 
-gulp.task('bacon',function(){
+  const sass = require('gulp-sass')(require('sass'));;
 
-// return gulp.src('app/scss/styles.scss')
-// 	.pipe(sass())
-// 	.pipe(gulp.dest('app/css'))
+function prefixify() {
+    gulp.src('./style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 99 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./'))
+}
 
-console.log("new task bacon is created");
+gulp.task('prefix_task', async function() {
+    prefixify();
 });
+
+function img() {
+    return gulp
+        .src("./uploads/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./minified"));
+}
+
+gulp.task("img_task", img);
+
+function sassify(){
+	return gulp.src('./mystyle.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./'))
+}
+
+gulp.task('sass',async function(){
+
+sassify();
+});
+
+gulp.task('watch',function(){
+
+    gulp.watch('./*',gulp.series(['sass']));
+    });
+    
+    gulp.task("default", gulp.series("sass","prefix_task",  "watch"));
